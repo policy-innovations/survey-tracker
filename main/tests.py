eed598986aaf50afdbaea79ae3ab673213e53112
Rohan Jain <crodjer@gmail.com>
2011-05-25 11:06:52 +0530

    initialized tests

--- main/tests.py
+++ main/tests.py
@@ -7,17 +7,29 @@ Replace these with more appropriate tests for your application.
 
 from django.test import TestCase
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
+from main.models import Person
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
+class PersonTreeTest(TestCase):
+    def setUp(self):
+        #head = Person.objects.create(name='A')
+        #s1 = Person.objects.create(name='B', supervisor=head)
+        #s2 = Person.objects.create(name='C', supervisor=head)
+        #ss1 = Person.objects.create(name='D', supervisor=s1)
+        #ss2 = Person.objects.create(name='E', supervisor=s1)
+        #ss3 = Person.objects.create(name='F', supervisor=s1)
+        #ss4 = Person.objects.create(name='G', supervisor=s2)
+        #sss1 = Person.objects.create(name='H', supervisor=ss1)
+        #sss2 = Person.objects.create(name='I', supervisor=ss3)
+        #sss3 = Person.objects.create(name='J', supervisor=ss4)
+        #sss4 = Person.objects.create(name='K', supervisor=ss4)
 
->>> 1 + 1 == 2
-True
-"""}
+        ## Use initial_data.json for setup instead
+        pass
 
+    def test_base(self):
+        #Getting the root persons
+        head = Person.objects.filter(supervisor=None)
+        subs = head[0].get_descendants()
+        ##Find out the leaf nodes of the tree
+        #lowest_children = subs.objects.filter()
+        self.assertEqual(subs.count(), 10)
