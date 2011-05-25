f191aae9f1c7fc46f80fbfa3d8e7a8ab8e206f9f
Rohan Jain <crodjer@gmail.com>
2011-05-26 00:30:58 +0530

    hierarchy based role qs

--- main/admin.py
+++ main/admin.py
@@ -1,4 +1,7 @@
+from django.db.models import Q
+
 from django.contrib import admin
+
 from mptt.admin import MPTTModelAdmin
 from main.models import *
 
@@ -8,13 +11,28 @@ class ProjectAdmin(ObjectPermissionMixin, admin.ModelAdmin):
     inlines = [ObjectPermissionInline]
 
 class RoleAdmin(MPTTModelAdmin):
-    list_display = ['name', 'level', 'head', 'get_descendant_count',
-                    'get_root',]
+    list_display = ['name', 'user', 'level', 'head', 'get_root',]
+    list_filter = ['user', 'project', 'level']
     MPTT_ADMIN_LEVEL_INDENT = 20
 
-    def queryset(self, *args, **kwargs):
-        qs = super(RoleAdmin, self).queryset(*args, **kwargs)
-        return qs.filter()
+    def queryset(self, request, *args, **kwargs):
+        qs = super(RoleAdmin, self).queryset(request, *args, **kwargs)
+        if request.user.is_superuser:
+            return qs
+        else:
+            self_roles = qs.filter(user=request.user)
+            query = Q()
+            for role in self_roles:
+                left = role.lft
+                right = role.rght
+                #Filter roles which are under the logged in user
+                q = Q(tree_id=role._mpttfield('tree_id'),
+                      lft__gte=left,
+                      rght__lte=right
+                      )
+                query |= q
+
+            return qs.filter(query)
 
 class ErrorTypeAdmin(MPTTModelAdmin):
     list_display = ['name', 'level', 'parent', 'get_descendant_count',
