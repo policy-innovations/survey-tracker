e9b71637056a002c7c86eddd984140b6a59da894
Rohan Jain <crodjer@gmail.com>
2011-05-31 01:36:49 +0530

    an error form with proper validation

--- main/forms.py
+++ main/forms.py
@@ -1,4 +1,7 @@
 from django import forms
+from django.db.models import Q, F
+
+from mptt.forms import TreeNodeChoiceField
 from main.models import *
 
 class UIDStatusForm(forms.ModelForm):
@@ -10,12 +13,45 @@ class UIDErrorForm(forms.ModelForm):
     choices = [('','-----------'),]
     for e in ErrorType.objects.all().filter(level=0):
         choices.append((str(e.pk), e.name))
-    print len(choices)
     etype = forms.ChoiceField(label="Errors", widget=forms.Select(attrs={
         'class':'error_types'}), choices=choices)
     class Meta:
         model = UIDError
         exclude = ('uid_status',)
+
+class ErrorForm(forms.ModelForm):
+    '''
+    This is an error form which is to be filled in for associating errors
+    with a UID.
+    Call it via ajax for error.
+    '''
+    class Meta:
+        model = UIDError
+
+    def __init__(self, role, *args, **kwargs):
+        super(ErrorForm, self).__init__(*args, **kwargs)
+        self.fields['uid_status'].queryset = role.uids()
+        project = role.get_project()
+        query = Q()
+        # This generates a query which filters trees from error type
+        # according to the project's linked error types
+        for et in project.error_types.all():
+            q = Q(lft__gte=et.lft, rght__lte=et.rght)
+            query |= q
+        self.fields['etype'].queryset = ErrorType.objects.filter(query)
+
+    def clean_etype(self):
+        et = self.cleaned_data['etype']
+        if et.is_leaf_node():
+            return self.cleaned_data['etype']
+        else:
+            raise forms.ValidationError("A leaf node is required.")
+
+# This is yet to be completed
+class UIDForm(forms.Form):
+    completer = forms.ModelChoiceField(queryset=Role.objects.all())
+    error_types = forms.ModelChoiceField(queryset=ErrorType.objects.all())
+
 
 class ProjectAdminForm(forms.ModelForm):
     hierarchy = forms.ModelChoiceField(queryset=Role.objects.filter(head__isnull=True))
--- main/tests.py
+++ main/tests.py
@@ -6,7 +6,9 @@ Replace these with more appropriate tests for your application.
 """
 
 from django.test import TestCase
-from main.models import Role
+
+from main.models import Role, ErrorType, Project, UIDStatus
+from main.forms import ErrorForm
 
 
 class RoleTreeTest(TestCase):
@@ -16,10 +18,21 @@ class RoleTreeTest(TestCase):
         pass
 
     def test_base(self):
-        #Getting the root persons
+        # Getting the root persons
         head = Role.tree.root_nodes()
         subs = head[0].get_descendants()
-        ##Find out the leaf nodes of the tree
+        # Find out the leaf nodes of the tree
         leaf_nodes = head[0].get_leafnodes()
-        print leaf_nodes
-        self.assertEqual(leaf_nodes.count(), 7)
+        self.assertEqual(leaf_nodes.count(), 5)
+
+class ErrorTest(TestCase):
+    def test_error_form(self):
+        role = Role.objects.get(pk=1)
+        f = ErrorForm(role, {'etype':1, 'uid_status':1})
+        # Wrong uid_status
+        self.assertEqual(f.is_valid(), False)
+        # Wrong error type
+        f = ErrorForm(role, {'etype':1, 'uid_status':3})
+        self.assertEqual(f.is_valid(), False)
+        f = ErrorForm(role, {'etype':3, 'uid_status':3})
+        self.assertEqual(f.is_valid(), True)
