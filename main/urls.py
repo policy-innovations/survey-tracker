894d1d242e0208ab9a8104e1e784b0893ac9e834
Pankaj Singh <contact@iampankaj.in>
2011-06-01 13:54:01 +0530

    added select surveyor page

--- main/urls.py
+++ main/urls.py
@@ -3,12 +3,12 @@ from django.conf.urls.defaults import patterns, url
 urlpatterns = patterns('',
     url(r'^$', 'main.views.home', name = 'home'),
     #url(r'^new-entry/$', 'main.views.new_entry', name = 'new_entry'),
-    url(r'^add-entry/(?P<proj_pk>\d+)/$', 'main.views.add_entry',
+    url(r'^select-surveyor/(?P<proj_pk>\d+)/$', 'main.views.select_surveyor',
+        name = 'select-surveyor'),
+    url(r'^add-entry/(?P<role_id>\d+)/$', 'main.views.add_entry',
         name = 'add-entry'),
     url(r'^manage-uids/(?P<role_id>\d+)/$', 'main.views.manage_uids',
         name = 'manage-uids'),
     url(r'^get-error-types/$', 'main.views.get_error_types',
         name = 'get_error_types'),
-    url(r'^get-errors/$', 'main.views.get_errors', name = 'get_errors'),
-
 )
