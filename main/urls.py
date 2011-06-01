dc61938b1071ea99503cc2304912f6d8bd7de24a
Rohan Jain <crodjer@gmail.com>
2011-06-01 11:52:20 +0530

    more cleanup

--- main/urls.py
+++ main/urls.py
@@ -1,13 +1,12 @@
 from django.conf.urls.defaults import patterns, url
 
-from main.models import * 
 urlpatterns = patterns('',
-        url(r'^$', 'main.views.home', name = 'home'),
-        #url(r'^new-entry/$', 'main.views.new_entry', name = 'new_entry'),
-        url(r'^add-entry/(?P<proj_pk>\d+)/$', 'main.views.add_entry', name = 'add_entry'),
-        url(r'^get-error-types/$', 'main.views.get_error_types',
-            name = 'get_error_types'),
-        url(r'^get-errors/$', 'main.views.get_errors',
-            name = 'get_errors'),
+    url(r'^$', 'main.views.home', name = 'home'),
+    #url(r'^new-entry/$', 'main.views.new_entry', name = 'new_entry'),
+    url(r'^add-entry/(?P<proj_pk>\d+)/$', 'main.views.add_entry',
+        name = 'add_entry'),
+    url(r'^get-error-types/$', 'main.views.get_error_types',
+        name = 'get_error_types'),
+    url(r'^get-errors/$', 'main.views.get_errors', name = 'get_errors'),
 
 )
