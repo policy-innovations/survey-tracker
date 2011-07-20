632736aff8ddff67b50f586c6cf13e5b1c84a048
Rohan Jain <crodjer@gmail.com>
2011-07-20 18:42:23 +0530

    Epio settings

--- production_settings.py
+++ production_settings.py
@@ -1,4 +1,11 @@
 from settings import *
+import os
+
+LIB_PATH = os.path.join(ROOT_PATH, 'lib')
+
+for directory in os.listdir(LIB_PATH):
+    sys.path.insert(0, os.path.join(LIB_PATH, directory))
+
 
 DEBUG = False
 SITE_ID = 2
